876e91fece6fedefe334bcfdf064fc176e1f7fe7
kdyang <xzreder@gmail.com>
2025-08-25 21:46:42 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# autodl-helper
+
+This is a simple initialization script for the autodl compute platform.
+
+在申请到算力平台的实例后，先运行本脚本，可以让后续的使用更加轻松便捷。
+
+```bash
+source autodl_config.sh
+```
---
+++ autodl_config.sh
@@ -0,0 +1,27 @@
+# install zsh
+sudo apt update && sudo apt upgrade -y
+sudo apt install zsh
+
+# install oh-my-zsh
+sh -c "$(curl -fsSL https://install.ohmyz.sh/)"
+
+# append conda into .zshrc
+cat <<'EOF' >> /root/.zshrc
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+__conda_setup="$('/root/miniconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
+if [ $? -eq 0 ]; then
+    eval "$__conda_setup"
+else
+    if [ -f "/root/miniconda3/etc/profile.d/conda.sh" ]; then
+        . "/root/miniconda3/etc/profile.d/conda.sh"
+    else
+        export PATH="/root/miniconda3/bin:$PATH"
+    fi
+fi
+unset __conda_setup
+# <<< conda initialize <<<
+EOF
+
+# append network settings into .zshrc
+echo 'source /etc/network_turbo' >> /root/.zshrc
